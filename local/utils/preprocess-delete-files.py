87804eab7fe8eee7003ef37b60950298191e4a77
Uwe Steinmann <uwe@steinmann.cx>
2024-10-28 14:55:56 +0100

    log url

--- local/utils/preprocess-delete-files.py
+++ local/utils/preprocess-delete-files.py
@@ -44,6 +44,7 @@ def run(input_files):
             biblio_number = input_file.readline()
 
             url = "{0}/api/v1/search?lookfor=biblio_no:{1}&type=AllFields".format(server_url, biblio_number.rstrip())
+            logger.info(url)
 
             req = urllib.request.Request(url)
 
